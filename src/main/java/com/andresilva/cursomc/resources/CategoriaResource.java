3fb48d9f8ba278e792d48344e77815b5c85e9660
andre silva <andremateusilva@gmail.com>
2018-10-25 18:10:08 +0100

    tested the first of domain class - categoria

--- src/main/java/com/andresilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andresilva/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,13 @@
 package com.andresilva.cursomc.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.andresilva.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias") // na boa pratica o rest coloca-se o verbo do end point no plural
@@ -11,7 +15,15 @@ public class CategoriaResource {
 	
 	// na boa prtica do rest deve-se identificar correctamente os verbos http para cada operação que queremos neste caso o Get 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta a funcionar";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 }
