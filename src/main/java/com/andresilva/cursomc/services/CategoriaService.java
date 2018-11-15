f676856836692fc2465e9d0ffd8394c60c787dfc
andre silva <andremateusilva@gmail.com>
2018-11-15 18:41:24 +0000

    Apagando uma categoria com DELETE

--- src/main/java/com/andresilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/andresilva/cursomc/services/CategoriaService.java
@@ -2,10 +2,12 @@ package com.andresilva.cursomc.services;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.andresilva.cursomc.domain.Categoria;
 import com.andresilva.cursomc.repositories.CategoriaRepository;
+import com.andresilva.cursomc.services.exceptions.DataIntegrityException;
 import com.andresilva.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,7 +34,17 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria categoria) {
-		//find(categoria.getId());
+		find(categoria.getId());
+		//O métedo save é usado para gravar novos registos e também para actualizar
 		return repository.save(categoria);
+	}
+	
+	public void delete(Integer id) {
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que tem produtos!!");
+		}
+		
 	}
 }
