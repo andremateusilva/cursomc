17d540fc03be6a5fb81916118cd1cdd961e86613
andre silva <andremateusilva@gmail.com>
2018-11-15 16:49:11 +0000

    Adicionei metedo POST para adicionar uma nova Categoria

--- src/main/java/com/andresilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andresilva/cursomc/resources/CategoriaResource.java
@@ -1,12 +1,16 @@
 package com.andresilva.cursomc.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andresilva.cursomc.domain.Categoria;
 import com.andresilva.cursomc.services.CategoriaService;
@@ -31,10 +35,26 @@ public class CategoriaResource {
 	@Autowired
 	CategoriaService service;
 	
+	//GET
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> listar(@PathVariable Integer id) {
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 
 		Categoria categoria = service.buscar(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+	
+	//POST
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = service.insert(categoria);
+		
+		/*
+		 * BOA PRÁTICA REST: após ser feita a inseerção num banco dados deve ser dada uma resposta com codigo de 
+		 * sucesso (201) e dar o URI do objecto adicionado
+		 */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/andresilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/andresilva/cursomc/services/CategoriaService.java
@@ -1,7 +1,6 @@
 package com.andresilva.cursomc.services;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +20,14 @@ public class CategoriaService {
 		//return categoria.orElse(null);
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(    
 				"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName())); 
+	}
+	
+	public Categoria insert(Categoria categoria) {
+		/*
+		 * usamos o setId a null para garantir que o id(a nova categoria) ainda não existe, pois caso já exista
+		 *  o metedo save() iria considerar que era uma actualização e actualiza em vez de inserir...
+		 */
+		categoria.setId(null);
+		return repository.save(categoria);
 	}
 }
