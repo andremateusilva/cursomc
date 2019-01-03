5d09569bb8da8db8683aee1e0caed74ff9344197
andre silva <andremateusilva@gmail.com>
2019-01-03 16:52:33 +0000

    PUT, GET E DELETE para cliente

--- src/main/java/com/andresilva/cursomc/resources/ClientesResource.java
+++ src/main/java/com/andresilva/cursomc/resources/ClientesResource.java
@@ -1,13 +1,24 @@
 package com.andresilva.cursomc.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andresilva.cursomc.domain.Cliente;
+import com.andresilva.cursomc.dto.ClienteDTO;
 import com.andresilva.cursomc.services.ClienteService;
 
 @RestController
@@ -17,14 +28,67 @@ public class ClientesResource {
 	// 1 - Criar camada service (que vai comunicar com repository)
 	// 2 - declarar a propriedade service
 	@Autowired
-	ClienteService clienteService;
+	ClienteService service;
 	
 	// Métedos de acesso à camada service para obter os dados de service (que posteriormente comunica com o repository)
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> listar(@PathVariable Integer id) {
-		Cliente cliente = clienteService.buscar(id);
+		Cliente cliente = service.buscar(id);
 		
 		return ResponseEntity.ok().body(cliente);
 	}
-
+	
+	/////////////////////////////////////////////////////
+	
+	
+	//GET All Clientes
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<?> findAll() {
+		List<Cliente> categorias = service.findAll();
+		
+		// o metedo .collect(Collectors.toList()); serve para converte/fazer cast novamente para o list
+		List<ClienteDTO> categoriaDTOs = categorias.stream().map(ob -> new ClienteDTO(ob)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(categoriaDTOs);
+	}
+		
+	//PUT
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO categoriaDTO, @PathVariable Integer id) {
+		Cliente categoria = service.fromDTO(categoriaDTO);
+		categoria.setId(id);
+		categoria = service.update(categoria);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	// Métedo para paginar os dados fornecidos (neste caso as categorias)
+	/*
+	 * neste uri vai ser construido como uma query string Ex: http://localhost/categorias?page=10&linesPerPage=1....etc
+	 * para isso usamos a anotação @RequestParam(value = "linesPerPage", defaultValue = "24") o valor é o nome da variavel 
+	 * para a a query string e o defaultValue é para que os paremetros possam ser OPTIONAL 
+	 * NOTA: no linesPage usamos os defaultValue é 24, porque 24 é multiplo de 1, 2, 3 e 4 .... e assim é mais facil 
+	 * organizar os dados de forma responsiva numa pagina conforme o tamanho....1 em 1, 2 em 2, 3 em 3
+	 */
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction) 
+	{
+		
+		Page<Cliente> categorias = service.findPage(page, linesPage, orderBy, direction);
+		
+		// o metedo .collect(Collectors.toList()); serve para converte/fazer cast novamente para o list
+		//List<ClienteDTO> categoriaDTOs = categorias.stream().map(ob -> new ClienteDTO(ob)).collect(Collectors.toList());
+		Page<ClienteDTO> categoriaDTOs = categorias.map(ob -> new ClienteDTO(ob));
+		return ResponseEntity.ok().body(categoriaDTOs);
+	}
 }
--- src/main/java/com/andresilva/cursomc/services/ClienteService.java
+++ src/main/java/com/andresilva/cursomc/services/ClienteService.java
@@ -1,13 +1,22 @@
 package com.andresilva.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.andresilva.cursomc.domain.Categoria;
 import com.andresilva.cursomc.domain.Cliente;
+import com.andresilva.cursomc.domain.Cliente;
+import com.andresilva.cursomc.dto.ClienteDTO;
 import com.andresilva.cursomc.repositories.ClienteRepository;
+import com.andresilva.cursomc.services.exceptions.DataIntegrityException;
 import com.andresilva.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,13 +24,54 @@ public class ClienteService {
 	
 	//delcarar propriedade clienteRepository que vais er responsavelpor dar os dados
 	@Autowired
-	ClienteRepository clienteRepository;
+	ClienteRepository repository;
 	
 	public Cliente buscar(Integer id) {
-		Optional<Cliente> cliente = clienteRepository.findById(id);
+		Optional<Cliente> cliente = repository.findById(id);
 		
 		return cliente.orElseThrow(() -> new ObjectNotFoundException(    
-				"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public List<Cliente> findAll() {
+		List<Cliente> clientes = repository.findAll();
+		Optional<List<Cliente>> optionalCliente = Optional.of(clientes);
+		
+		return optionalCliente.orElseThrow(() -> new ObjectNotFoundException(    
+				"List não encontrada!!!"));
 	}
 
+	public Cliente update(Cliente cliente) {
+		Cliente newCliente = buscar(cliente.getId());
+		updateData(newCliente, cliente);
+		
+		return repository.save(newCliente);
+	}
+	
+	public void delete(Integer id) {
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas!!");
+		}
+		
+	}
+	
+	// Métedo para paginar os dados fornecidos (neste caso as clientes)
+	// PAGE é uma class do Spring.data
+	public Page<Cliente> findPage(Integer page, Integer linesPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		
+		return repository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 }
